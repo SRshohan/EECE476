cb758eab316271df39b28dca47d849071682bc9e
SRshohan <srshohan02@gmail.com>
2024-10-01 18:06:09 -0400

    Added project 2

---
+++ project_2/person.h
@@ -0,0 +1,25 @@
+// person.h
+
+#ifdef PERSON_H
+#define PERSON_H
+
+
+class person
+{
+private:
+    /* data */
+public:
+    person(/* args */);
+    ~person();
+};
+
+person::person(/* args */)
+{
+}
+
+person::~person()
+{
+}
+
+
+#endif 
