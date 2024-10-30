670ec915303705c14e132326a5cc9568062e1c1f
SRshohan <srshohan02@gmail.com>
2024-10-29 21:08:50 -0400

    Added files

---
+++ class_ex_5/reverse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+// •Design a recursive algorithm in pseudo-code to print a 
+// given array with n elements in reverse order. For example, 
+// if the input is [3, 5, 1, 7, 9], then the output is 9, 7, 1, 5, 3.
+
+int reverse(int[] nums){
+    if 
+}
+    
+
+
