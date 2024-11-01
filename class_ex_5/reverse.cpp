3bfe22b936cc53bab40c6f674d514ebcca1a314f
SRshohan <srshohan02@gmail.com>
2024-10-31 22:44:26 -0400

    Working on this asssignment

--- class_ex_5/reverse.cpp
+++ class_ex_5/reverse.cpp
@@ -6,8 +6,17 @@ using namespace std;
 // given array with n elements in reverse order. For example, 
 // if the input is [3, 5, 1, 7, 9], then the output is 9, 7, 1, 5, 3.
 
-int reverse(int[] nums){
-    if 
+void reverse(int nums[], int siz){
+    if (siz == 0){
+        return;
+    }
+
+    reverse(nums, siz - 1);
+};
+
+int main(){
+
+
 }
     
 
