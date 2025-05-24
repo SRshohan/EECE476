aa584d99265403295e0b75efaeb7fc24df4d33ab
SRshohan <srshohan02@gmail.com>
2025-05-23 22:52:21 -0400

    Added

---
+++ dsa/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    printf("Hello World!\n");
+    char arr[100];
+    printf("Enter the number of elements: ");
+    scanf("%s", &arr);
+    printf("You entered: %s\n", arr);
+    
+}
